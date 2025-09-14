cb077d8641d2b27313ab40a866a7c397499e7579
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-09-14 11:46:20 +0300

    function

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 
 function Signup() {
 
@@ -8,17 +9,41 @@ function Signup() {
   const [error,setError] = useState("")
   const [loading,setLoading] = useState("")
 
+  const navigate = useNavigate()
+
+  const {session, signUpNewUser} = useAuth()
+
+  console.log(session)
+
+  async function  handleSignUp(e){
+    e.preventDefault()
+
+    setLoading(true)
+
+    try {
+      const result = await signUpNewUser(email, password)
+      if(result.success){
+        navigate("/dashboard")
+      }
+    } catch (error) {
+      setError("an error occurred", error)
+    } finally{
+      setLoading(false)
+    }
+  }
+
   return (
     <div>
-      <form  className="max-w-md m-auto pt-24">
+      <form onSubmit={handleSignUp}  className="max-w-md m-auto pt-24">
         <h2 className="font-bold pb-2">Sign up today!</h2>
         <p>Already have an account 
           <Link to="/signup">Sign up</Link>
         </p>
         <div className="flex flex-col py-4">
-          <input placeholder="Email" className="p-3 mt-6 " type="text" />
-          <input placeholder="Password" className="p-3 mt-6 " type="password" />
+          <input onChange={(e)=>setEmail(e.target.value)} placeholder="Email" className="p-3 mt-6 " type="text" />
+          <input onChange={(e)=>setPassword(e.target.value)} placeholder="Password" className="p-3 mt-6 " type="password" autoComplete="new-password" />
           <button type="submit" disabled={loading} className="mt-6">Sign up</button>
+          {error && <p className="text-red-600 text-center pt-4">{error}</p>}
         </div>
       </form>
     </div>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { supabase } from "../supabase_client";
 
 const AuthContext = createContext(undefined)
 
@@ -6,14 +7,68 @@ export function AuthContextProvider ({children}){
 
     const [session, setSession] = useState(undefined)
 
+    async function signUpNewUser(email, password) {
+        const {data,error} = await supabase.auth.signUp({email, password})
+
+        if(error){
+            console.error("there was a problem signup :",error)
+
+            return {success: false, error}
+        }
+
+            return {success: true, data}
+    }
+
+    async function signInUser(email, password) {
+        try {
+            const {data, error} = await supabase.auth.signInWithPassword({email, password})
+
+            if(error){
+                console.error("there was a problem signin", error.message)
+            return {success: false, data}
+        }
+        return {success: true , data}
+
+        } catch (error) {
+        console.error("an error occurred", error)
+        }
+
+    }
+
+    async function signOut(){
+        const {data, error} = await supabase.auth.signOut()
+
+        if(error){
+            console.error("there was a problem signout", error)
+            return {success: false, data}
+        }
+        return {success: true , data}
+    }
+
+
+
+    useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+        setSession(session);
+    });
+
+    const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session);
+    });
+
+    return () => {
+        authListener.subscription?.unsubscribe();
+    };
+}, []);
+
 
     return (
-        <AuthContext.Provider value={{session}} >
+        <AuthContext.Provider value={{session, signUpNewUser, signInUser, signOut}} >
         {children}
     </AuthContext.Provider>
     )
 }
 
-export const useAuth = () =>{
+export const useAuth = ()=>{
     return useContext(AuthContext)
 } 
